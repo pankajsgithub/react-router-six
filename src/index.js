d410f035d5a60b7a95a822738967bd865dbdf33e
pankajsgithub <pankaj.summerproject2016@gmail.com>
2022-07-10 19:44:18 +0530

    added Navigate Element usage

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/myapps' />
+      <Route path='/myapps' element={<Navigate replace to={'/learn'} />} />
+      <Route path='/learn' element={<Learn />} />
     </Routes>
   </Router>
 );
