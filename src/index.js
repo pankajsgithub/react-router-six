6b278873855f0e1aa1dc4c0e52d5424450d9fc54
pankajsgithub <pankaj.summerproject2016@gmail.com>
2022-07-10 19:24:28 +0530

    Add files

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/myapps' />
+    </Routes>
+  </Router>
+);
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Route</h1>
+    </div>
+  )
+}
+
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed here</h4>
+    </div>
+  )
+}
+
+reportWebVitals();
